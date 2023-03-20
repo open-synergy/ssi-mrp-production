537f3a65b6d68c9e3ac1285baa15e8608568aae9
mikevhe18 <viriyananda.michael@gmail.com>
2023-03-20 15:11:09 +0700

    [ssi_mrp_production] 14.0.1.0.0 QC

--- ssi_mrp_production/__manifest__.py
+++ ssi_mrp_production/__manifest__.py
@@ -1,16 +1,13 @@
-# Copyright 2015-2016 Agile Business Group (<http://www.agilebg.com>)
-# Copyright 2015 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
-#    Author Marco Dieckhoff
-# Copyright 2022 OpenSynergy Indonesia
-# Copyright 2022 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "MRP Production",
     "version": "14.0.1.0.0",
     "category": "Manufacturing/Manufacturing",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "https://simetri-sinergi.id",
     "depends": [
         "mrp",
--- ssi_mrp_production/models/__init__.py
+++ ssi_mrp_production/models/__init__.py
@@ -1,10 +1,6 @@
-# Copyright 2015-2016 Agile Business Group (<http://www.agilebg.com>)
-# Copyright 2015 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
-#    Author Marco Dieckhoff
-# Copyright 2022 OpenSynergy Indonesia
-# Copyright 2022 PT. Simetri Sinergi Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from . import (
     mrp_production,
 )
